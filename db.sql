514b3f68175f24c86c6c4f848054606b8dfe05f4
Trapenok Victor <levhav@yandex.ru>
2018-01-25 01:01:45 +1000

    v3.36

--- db.sql
+++ db.sql
@@ -1,13 +1,23 @@
 -- phpMyAdmin SQL Dump
 -- version 3.4.11.1deb2+deb7u8
 -- http://www.phpmyadmin.net
--- 
+--
+-- Хост: test.comet.su:3305
+-- Время создания: Янв 24 2018 г., 14:56
+-- Версия сервера: 5.5.57
+-- Версия PHP: 5.6.31-1~dotdeb+7.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
--- База данных: `comet_db`
+-- База данных: `comet_db_saas`
 --
 
 -- --------------------------------------------------------
@@ -30,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `conference` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Кому и к какой конференции дан доступ';
 
 -- --------------------------------------------------------
- 
+
 --
 -- Структура таблицы `log_event`
 --
@@ -39,7 +49,7 @@ CREATE TABLE IF NOT EXISTS `log_event` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `text` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1637 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1661 ;
 
 -- --------------------------------------------------------
 
@@ -93,6 +103,19 @@ CREATE TABLE IF NOT EXISTS `pipe_messages` (
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `revoked_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS `revoked_tokens` (
+  `dev_id` int(11) NOT NULL,
+  `token` varbinary(600) NOT NULL,
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`dev_id`,`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users_auth`
 --
 
@@ -114,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `users_auth` (
 CREATE TABLE IF NOT EXISTS `users_data` (
   `dev_id` int(9) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `data` varbinary(600) NOT NULL,
   PRIMARY KEY (`dev_id`,`user_id`),
   KEY `dev_id` (`dev_id`),
   KEY `user_id` (`user_id`)
@@ -154,3 +177,7 @@ CREATE TABLE IF NOT EXISTS `users_time` (
   KEY `dev_id` (`dev_id`),
   KEY `user_id` (`user_id`)
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
